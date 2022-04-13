9444468d83288ccfb353194daa1bdf832c79fc22
Ciaran McKenna <hello@ciaranmckenna.com>
2022-04-13 12:50:09 +0100

    Specifying foreign key column

--- company-coordinator-app/src/main/java/com/ciaranmckenna/companycoordinatorapp/model/Application.java
+++ company-coordinator-app/src/main/java/com/ciaranmckenna/companycoordinatorapp/model/Application.java
@@ -5,6 +5,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
@@ -20,6 +21,7 @@ public class Application {
     private String description;
 
     @ManyToOne
+    @JoinColumn(name = "organization_id")
     private Organization organization;
 
     public Application() {
