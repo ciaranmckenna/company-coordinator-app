564664aeadb6730c0b0fa8b4c17b766cf91ad2b6
Ciaran McKenna <hello@ciaranmckenna.com>
2022-04-13 10:19:45 +0100

    Project setup

---
+++ company-coordinator-app/src/main/java/com/ciaranmckenna/companycoordinatorapp/CompanyCoordinatorAppApplication.java
@@ -0,0 +1,13 @@
+package com.ciaranmckenna.companycoordinatorapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CompanyCoordinatorAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CompanyCoordinatorAppApplication.class, args);
+	}
+
+}
---
+++ company-coordinator-app/src/test/java/com/ciaranmckenna/companycoordinatorapp/CompanyCoordinatorAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ciaranmckenna.companycoordinatorapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CompanyCoordinatorAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
